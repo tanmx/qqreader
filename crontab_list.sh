41913d5b75b4ec24836f8012a2a1e9226fcf41f1
tanmx <tanmingxiao@gmail.com>
2021-01-05 22:20:14 +0800

    Update crontab_list.sh

--- crontab_list.sh
+++ crontab_list.sh
@@ -1,9 +1,9 @@
 #必须要的默认定时任务请勿删除
-52 */1 * * * sh /qx-scripts/default_task.sh >> /scripts/logs/default_task.log 2>&1
+52 */1 * * * sh /qx-scripts/default_task.sh >> /qx-scripts/logs/default_task.log 2>&1
 # 每3天的23:50分清理一次日志
 50 23 */3 * * rm -rf /qx-scripts/logs/*.log
 
 #聚看点
-*/20 */2 * * * node /qx-scripts/Sunert-Scripts/Task/jukan.js >> /scripts/logs/jukan.log 2>&1
+*/20 */2 * * * node /qx-scripts/Sunert-Scripts/Task/jukan.js >> /qx-scripts/logs/jukan.log 2>&1
 #抖音极速版
-*/5 * * * * node /qx-scripts/ZhiYi-N-script/dyjsb.js >> /scripts/logs/dyjsb.log 2>&1
+*/5 * * * * node /qx-scripts/ZhiYi-N-script/dyjsb.js >> /qx-scripts/logs/dyjsb.log 2>&1
