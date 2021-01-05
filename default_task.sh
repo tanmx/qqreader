a5d7a3cefb3f8aac0fd630a17b565d4268b54446
tanmx <tanmingxiao@gmail.com>
2021-01-05 18:40:37 +0800

    Update default_task.sh

--- default_task.sh
+++ default_task.sh
@@ -1,6 +1,6 @@
 echo "定时任务更新代码，git 拉取最新代码，并安装更新依赖..."
 git -C /qx-scripts pull
-for i in Sunert-Scripts; do
+for i in Sunert-Scripts ZhiYi-N-script; do
 git -C /qx-scripts/${i} pull
 npm install --prefix /qx-scripts/${i}
 done
