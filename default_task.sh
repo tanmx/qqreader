b5a5ff96886d435ecba7be8f0b2313ed0cfabb35
tanmx <tanmingxiao@gmail.com>
2021-01-06 11:39:16 +0800

    更新文件夹判断

--- default_task.sh
+++ default_task.sh
@@ -14,12 +14,12 @@ do
 done < ${CONFIG}
 
 echo "定时任务更新代码，git 拉取最新代码，并安装更新依赖..."
-while read LINE
+for i in `ls /qx-scripts/repositories/`
 do
-  NAME=`echo $LINE | awk '{print $1}'`
-  REPO=`echo $LINE | awk '{print $2}'`
-  git -C /qx-scripts/repositories/${NAME}-${REPO} pull
-  npm install --prefix /qx-scripts/repositories/${NAME}-${REPO}
-done < ${CONFIG}
+  if [ -d /qx-scripts/repositories/${i} ]；then
+	git -C /qx-scripts/repositories/${i} pull
+	npm install --prefix /qx-scripts/repositories/${i}
+  fi
+done
 echo "加载最新task"
 crontab /qx-scripts/crontab_list.sh
