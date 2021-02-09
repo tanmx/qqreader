9afd3bf0fcad8dd03e93409fe6bd6a84c9ccabf4
tanmx <tanmingxiao@gmail.com>
2021-02-09 10:04:06 +0800

    增加百度极速版

--- crontab_list.sh
+++ crontab_list.sh
@@ -7,7 +7,7 @@
 */20 */2 * * * node /qx-scripts/repositories/Sunert-Scripts/Task/jukan.js >> /qx-scripts/logs/jukan.log 2>&1
 #中青看点
 */7 */2 * * * node /qx-scripts/repositories/Sunert-Scripts/Task/youth.js >> /qx-scripts/logs/youth.log 2>&1
+#百度极速版
+*/30 * * * * node /qx-scripts/repositories/Sunert-Scripts/Task/baidu_speed.js >> /qx-scripts/logs/baidu_speed.log 2>&1
 #喜玛拉雅
 */30 * * * * python3 /qx-scripts/repositories/Zero-S1-xmly_speed/xmly_speed.py >> /qx-scripts/logs/xmly_speed.log 2>&1
-#笑谱视频
-*/15 8-23 * * * node /qx-scripts/repositories/ziye12-JavaScript/Task/iboxpay.js >> /qx-scripts/logs/iboxpay.log 2>&1
