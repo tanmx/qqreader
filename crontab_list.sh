8f6f782a36978377054df54b4ddc09f9651cacba
tanmx <tanmingxiao@gmail.com>
2021-03-29 10:56:28 +0800

    Update crontab_list.sh

--- crontab_list.sh
+++ crontab_list.sh
@@ -16,7 +16,7 @@
 #今日头条极速版
 5,35 8-23 * * * node /qx-scripts/repositories/ZhiYi-N-Private-Script/Scripts/jrtt.js >> /qx-scripts/logs/jrtt.log 2>&1
 #快看点
-*/30 * * * * node /qx-scripts/repositories/ZhiYi-N-Private-Script/Scripts/kkd.js >> /qx-scripts/logs/kkd.log 2>&1
+#*/30 * * * * node /qx-scripts/repositories/ZhiYi-N-Private-Script/Scripts/kkd.js >> /qx-scripts/logs/kkd.log 2>&1
 #笑谱视频
 #*/6 * * * * node /qx-scripts/repositories/6Svip120apk69-gitee_q8qsTAUA_cThxc1RBVUE/Task/iboxpay.js >> /qx-scripts/logs/iboxpay.log 2>&1
 #步步宝
